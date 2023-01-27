f4a5219f4a532570c2f90f1406eec655853a3eaa
ikaslea <ikaslea@DESKTOP-I2NMDJ9>
2023-01-27 14:29:30 +0100

    ende

---
+++ src/principal/Calculadora.java
@@ -0,0 +1,42 @@
+package principal;
+import menu.Menu;
+import operaciones.Operaciones;
+/**
+ * 
+ * @author ikaslea
+ * @version 1.0
+ */
+public class Calculadora{
+    public static void main(String[] args) {   
+        int resultado = 0;
+        String operacion = "";
+        int[] operandos = new int [2];
+        
+        Menu menu = new Menu();
+        Operaciones operaciones = new Operaciones();
+        
+        do{
+            operandos = menu.pedirNumeros();
+            operacion = menu.menuOpciones();
+            
+            if (operacion.equalsIgnoreCase("+")){
+                resultado = operaciones.sumar(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("-")){
+                resultado = operaciones.restar(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("*")){
+                resultado = operaciones.multiplicar(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("/")){
+                resultado = operaciones.dividir(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("%")){
+                resultado = operaciones.resto(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else {
+                System.out.println ("Operacion no valida");
+            }
+        }   while (menu.repetir());
+    }
+}
